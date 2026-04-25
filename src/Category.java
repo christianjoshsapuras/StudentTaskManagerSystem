931ed29209cdb58b21a416d778d2893cd026511d
Christian Josh Sapuras <155878865+christianjoshsapuras@users.noreply.github.com>
2026-04-25 17:52:07 +0800

    Update Category.java
    Add Category Getters and Setters Methods

--- src/Category.java
+++ src/Category.java
@@ -4,10 +4,20 @@ public class Category {
   private int categoryID;
   private String categoryName;
 
-public Category(int categoryID, String categoryName) {
-  this.categoryID = categoryID;
-  this.categoryName = categoryName;
-}
+  public Category(int categoryID, String categoryName) {
+    this.categoryID = categoryID;
+    this.categoryName = categoryName;
+  }
 
-  
+  public int getCategoryID() {
+    return categoryID;
+  }
+
+  public String getCategoryName() {
+    return categoryName;
+  }
+
+  public void setCategoryName(String categoryName) {
+    this.categoryName = categoryName;
+  } 
 }
