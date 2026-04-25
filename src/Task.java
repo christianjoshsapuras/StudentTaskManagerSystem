a416863dd9a565a5ce9b8868efb94306a34b18af
Christian Josh Sapuras <155878865+christianjoshsapuras@users.noreply.github.com>
2026-04-25 17:37:39 +0800

    Update Task.java
    Add Task class attributes and constructor

--- src/Task.java
+++ src/Task.java
@@ -1,1 +1,21 @@
+package taskmanager;
+import java.util.Date;
 
+public class Task {
+  
+	private int taskID;
+	private String title;
+	private String description;
+	private Date dueDate;
+	private boolean status;
+
+  
+	public Task(int taskID, String title, String description, Date dueDate) {
+		this.taskID = taskID;
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.status = false;
+  }
+
+}
